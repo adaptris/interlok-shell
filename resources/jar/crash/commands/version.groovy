4d0f5c61c6ad2b2b743ac5a2b17ca52122cb911b
mcwarman <matthew.warman@adaptris.com>
2016-08-12 13:54:03 +0100

    version.groovy Fix trailing linebreak

--- resources/jar/crash/commands/version.groovy
+++ resources/jar/crash/commands/version.groovy
@@ -1,17 +1,17 @@
-import com.adaptris.core.management.VersionReport;
-import java.lang.StringBuilder;
+import com.adaptris.core.management.VersionReport
+import org.crsh.cli.Command
+import org.crsh.cli.Usage
 
 class version {
-  @Usage("show the interlok version")
+  @Usage("Display Interlok version information")
   @Command
   Object main() {
     VersionReport r = VersionReport.getInstance();
     StringBuilder sb = new StringBuilder();
     sb.append("Version Information");
-    sb.append(System.lineSeparator())
     for (String modules : r.getReport()) {
-      sb.append("  " + modules);
       sb.append(System.lineSeparator())
+      sb.append("  " + modules);
     }
     return sb.toString();
   }
